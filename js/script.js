ca9415a31cfb4da803ff8473dce7e8bd6d2e95d0
Andrea Pacifico <pacifico.mensa@gmail.com>
2020-01-08 19:16:04 +0100

    done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 var listaCognomi = ["tombola", "pappalardo", "abate", "lobriola", "vernieri",];
 var primoCognome;
 var j = 0;
-var newLista = [];
+var k = 0;
+var newList = [];
 
 var userCognome = prompt("inserisci il tuo cognome");
 listaCognomi.push(userCognome);
@@ -9,13 +10,18 @@ listaCognomi.push(userCognome);
 while (listaCognomi.length > 0) {
   primoCognome = listaCognomi[0];
   j = 0;
-  for (var i = 1; i <= listaCognomi.length; i++) {
+  for (var i = 0; i < listaCognomi.length; i++) {
     if (listaCognomi[i] < primoCognome) {
       primoCognome = listaCognomi[i];
       j = i;
     }
   }
-  newLista.push(primoCognome);
+  newList.push(primoCognome);
+  if (primoCognome == userCognome) {
+    k = newList.length;
+  }
   listaCognomi.splice(j, 1);
   document.getElementById('lista-cognomi').innerHTML += "<li>" + primoCognome + "</li>"
  }
+
+document.getElementById('posizione').innerHTML = userCognome + " si trova in " + k + "a posizione.";
